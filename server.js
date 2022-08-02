98170be99705e828b730b20f5986ab74814a510f
EspoirNorbert <espoirnorbert@gmail.com>
2022-08-02 20:43:06 +0100

    updated server.js

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ const path = require('path')
 
 const app = express()
 app.use('/',serveStatic(path.join(__dirname,'/dist')))
+app.get(/.*/ , function(req, res) {
+    res.sendFile(path.join(__dirname,'/dist/index.html'))
+})
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8090
 
-app.listen(port)
+app.listen(port , ()=> {
+    console.log("Server is runiing at http://localhsot:"+port );
+})
