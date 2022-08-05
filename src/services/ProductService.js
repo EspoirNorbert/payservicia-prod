57b1ff51e8c830a1e2d7100ef6ebdc5ed9ad53fc
EspoirNorbert <espoirnorbert@gmail.com>
2022-08-05 21:00:58 +0100

    Mise a jour de la route pour recuper les produits

--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -3,6 +3,6 @@ import httpCommon from "@/http-common"
 export default class ProductService {
 
     static  getProduit() {
-        return  fetch("http://localhost:8080/products.json");
+        return  fetch( `${window.location.origin}/products.json`);
     }
 }
