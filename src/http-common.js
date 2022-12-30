36f540d6b74a122157844870911275fe218bd801
EspoirNorbert <espoirnorbert@gmail.com>
2022-12-30 21:40:19 +0100

    Update api base url

--- src/http-common.js
+++ src/http-common.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: "https://nores-api-payement.deta.dev/",
+  baseURL: "https://nores-api-payement.deta.dev/v1.0/",
   headers: {
     "Content-type": "application/json"
   }
